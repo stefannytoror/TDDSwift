64524ad7d8f4e4c3d6e25963357ae1236f63c805
stefannytoror <stefannyrami@gmail.com>
2019-10-22 16:16:14 -0500

    Initial Commit

---
+++ Linked_TDD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Linked_TDD
+//
+//  Created by Stefanny Toro Ramirez on 10/21/19.
+//  Copyright © 2019 Stefanny Toro Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Linked_TDDTests/Linked_TDDTests.swift
@@ -0,0 +1,34 @@
+//
+//  Linked_TDDTests.swift
+//  Linked_TDDTests
+//
+//  Created by Stefanny Toro Ramirez on 10/21/19.
+//  Copyright © 2019 Stefanny Toro Ramirez. All rights reserved.
+//
+
+import XCTest
+@testable import Linked_TDD
+
+class Linked_TDDTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
